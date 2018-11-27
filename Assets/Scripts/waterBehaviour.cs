f9563408894194557e20500df7a2fde3a87ac64d
Unknown <bradley2.moss@live.uwe.ac.uk>
2018-11-27 10:57:31 +0000

    Changed light to torch

--- Assets/Scripts/waterBehaviour.cs
+++ Assets/Scripts/waterBehaviour.cs
@@ -6,7 +6,7 @@ public class waterBehaviour : MonoBehaviour {
     public float waterRiseingRate = 0.5f;
     public GameObject playerObj;
     private Player Player;
-    float max;
+    //float max;
     public float HeightTolearnce;
     // Use this for initialization
     void Start () {
@@ -20,12 +20,12 @@ public class waterBehaviour : MonoBehaviour {
     }
     void RaiseWaterLevel()
     {
-        max = Player.getYPos() + HeightTolearnce;
+        //max = Player.getYPos() + HeightTolearnce;
 
-        Mathf.Clamp(transform.position.y, 0.0f, max);
-        if (transform.position.y < max)
-        {
+        //Mathf.Clamp(transform.position.y, 0.0f, max);
+        //if (transform.position.y < max)
+        //{
             this.transform.Translate(new Vector3(0.0f, waterRiseingRate * Time.deltaTime, 0.0f));
-        }
+       // }
     }
 }
---
+++ Assets/Torch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    public float flicker = 0.1f;
+    public float flickerSpeed = 1.0f;
+    private float waitTime = 0;
+    private Light torchLight;
+
+    // Use this for initialization
+    private void Start()
+    {
+        torchLight = GetComponent<Light>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        waitTime += Time.deltaTime;
+
+        if (waitTime >= Random.Range(15, 30))
+        {
+            StartCoroutine(FlickeringOn());
+            waitTime = 0;
+        }
+
+        
+
+    }
+
+    private IEnumerator FlickeringOn()
+    {
+        torchLight.enabled = false;
+        yield return new WaitForSeconds(Random.Range(flicker, flickerSpeed));
+        torchLight.enabled = true;
+    }
+
+}
