ba9d77d9257b26fa43ca391ce9a4ca4774f8b0a6
Unknown <alexander2.stroud@live.uwe.ac.uk>
2018-11-27 22:45:30 +0000

    Added fade function to oxygen tank prefab -dunno how to make work

--- Assets/OxygenCanisterExplode.cs
+++ Assets/OxygenCanisterExplode.cs
@@ -7,11 +7,12 @@ public class OxygenCanisterExplode : MonoBehaviour {
     public GameObject explosion;
     private GameObject player;
     public GameObject newWall;
-
+    public GUITexture guiTexture;
 	// Use this for initialization
 	void Start ()
     {
         player = GameObject.FindWithTag("Player");
+        guiTexture = GetComponent<GUITexture>();
     }
 	
 	// Update is called once per frame
@@ -30,7 +31,8 @@ public class OxygenCanisterExplode : MonoBehaviour {
             {
                 Debug.Log(thing.name);
                 Instantiate(explosion, this.transform).transform.parent = null;
-                thing.gameObject.GetComponent<RespawnTank>().Deactive();
+                //thing.gameObject.GetComponent<RespawnTank>().Deactive();
+                Destroy(this.gameObject);
             }
             if (thing.tag == "Blockable")
             {
@@ -39,8 +41,21 @@ public class OxygenCanisterExplode : MonoBehaviour {
                 GameObject crumbleWall = Instantiate(newWall, thing.transform.position, Quaternion.identity);
                 crumbleWall.transform.eulerAngles = thing.transform.eulerAngles;
                 Destroy(thing);
+                StartCoroutine(Fade(0, 1f, .1f));
             }
         }
-
+    }
+    IEnumerator Fade(float start, float end, float length)
+    {
+        Color col = guiTexture.color;
+        for (float i = 0f; i < 10.0f; i += Time.deltaTime*(1/length))
+        {
+            Debug.Log("Start fade");
+            col.a = Mathf.Lerp(start, end, i);
+            //guiTexture.color = col;
+        }
+        yield return null;
+        //white screen fade
+        //change to win scene
     }
 }
